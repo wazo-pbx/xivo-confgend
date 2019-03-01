38de5f9510aaff813dfa24cd11c2a1c1054d97b7
Alexandre Fournier <afournier@wazo.io>
2019-03-01 10:24:53 -0500

    provd network: extract more values from the database

--- xivo_confgen/plugins/provd_conf.py
+++ xivo_confgen/plugins/provd_conf.py
@@ -4,9 +4,9 @@
 
 from __future__ import unicode_literals
 
-import textwrap
+import yaml
 
-from xivo_dao import init_db_from_config, default_config
+from xivo_dao import init_db_from_config
 from xivo_dao.helpers.db_utils import session_scope
 from xivo_dao.alchemy.provisioning import Provisioning
 from xivo_dao.alchemy.netiface import Netiface
@@ -29,14 +29,48 @@ class ProvdNetworkConfGenerator(object):
             return
         return result.address
 
+    def get_provd_net4_ip_rest(self, session):
+        result = session.query(Provisioning.net4_ip_rest).first()
+        if not result:
+            return
+        return result.net4_ip_rest
+
+    def get_provd_rest_port(self, session):
+        result = session.query(Provisioning.rest_port).first()
+        if not result:
+            return
+        return result.rest_port
+
+    def get_provd_http_port(self, session):
+        result = session.query(Provisioning.http_port).first()
+        if not result:
+            return
+        return result.http_port
+
     def generate(self):
+        config = {}
         with session_scope() as session:
-            address = self.get_provd_net4_ip(session) or self.get_netiface_net4_ip(session)
+            external_ip = self.get_provd_net4_ip(session) or self.get_netiface_net4_ip(session)
+            external_port = self.get_provd_http_port(session)
+            rest_ip = self.get_provd_net4_ip_rest(session)
+            rest_port = self.get_provd_rest_port(session)
 
-        if not address:
-            return ''
+            sections = {
+                'general': {
+                    'external_ip': external_ip,
+                    'http_port': external_port,
+                },
+                'rest_api': {
+                    'ip': rest_ip,
+                    'port': rest_port,
+                }
+            }
 
-        return textwrap.dedent('''\
-            general:
-                external_ip: {}
-        '''.format(address))
+            for section_name, section_value in sections.iteritems():
+                for option, value in section_value.iteritems():
+                    if value:
+                        if section_name not in config:
+                            config.update({section_name: {}})
+                        config[section_name][option] = value
+
+        return yaml.safe_dump(config, default_flow_style=False)
--- xivo_confgen/plugins/tests/test_provd_conf.py
+++ xivo_confgen/plugins/tests/test_provd_conf.py
@@ -6,9 +6,6 @@ import unittest
 import textwrap
 
 from mock import Mock, patch
-from xivo_dao.alchemy.provisioning import Provisioning
-from xivo_dao.alchemy.netiface import Netiface
-
 from xivo_confgen.generators.tests.util import assert_config_equal
 from ..provd_conf import ProvdNetworkConfGenerator
 
@@ -24,8 +21,16 @@ class TestProvdNetworkConf(unittest.TestCase):
         session_scope.__enter__ = Mock(return_value=Mock())
         session_scope.__exit__ = Mock(return_value=None)
 
-        with patch.object(self.generator, 'get_provd_net4_ip') as net4_ip:
-            net4_ip.return_value = '10.0.0.254'
+        with patch.object(self.generator, 'get_provd_net4_ip') as provd_net4_ip,\
+                patch.object(self.generator, 'get_provd_net4_ip_rest') as provd_net4_ip_rest,\
+                patch.object(self.generator, 'get_provd_http_port') as provd_http_port,\
+                patch.object(self.generator, 'get_provd_rest_port') as provd_rest_port,\
+                patch.object(self.generator, 'get_netiface_net4_ip') as netiface_net4_ip:
+            provd_net4_ip.return_value = '10.0.0.254'
+            provd_net4_ip_rest.return_value = None
+            provd_http_port.return_value = None
+            provd_rest_port.return_value = None
+            netiface_net4_ip.return_value = None
 
             value = self.generator.generate()
 
@@ -39,13 +44,18 @@ class TestProvdNetworkConf(unittest.TestCase):
         session_scope.__enter__ = Mock(return_value=Mock())
         session_scope.__exit__ = Mock(return_value=None)
 
-        with patch.object(self.generator, 'get_provd_net4_ip') as provd_net4_ip:
+        with patch.object(self.generator, 'get_provd_net4_ip') as provd_net4_ip,\
+                patch.object(self.generator, 'get_provd_net4_ip_rest') as provd_net4_ip_rest,\
+                patch.object(self.generator, 'get_netiface_net4_ip') as netiface_net4_ip,\
+                patch.object(self.generator, 'get_provd_http_port') as provd_http_port,\
+                patch.object(self.generator, 'get_provd_rest_port') as provd_rest_port:
             provd_net4_ip.return_value = None
+            provd_net4_ip_rest.return_value = None
+            provd_http_port.return_value = None
+            provd_rest_port.return_value = None
+            netiface_net4_ip.return_value = '10.0.0.250'
 
-            with patch.object(self.generator, 'get_netiface_net4_ip') as netiface_net4_ip:
-                netiface_net4_ip.return_value = '10.0.0.250'
-
-                value = self.generator.generate()
+            value = self.generator.generate()
 
         assert_config_equal(value, textwrap.dedent('''\
             general:
@@ -53,16 +63,71 @@ class TestProvdNetworkConf(unittest.TestCase):
         '''))
 
     @patch('xivo_confgen.plugins.provd_conf.session_scope')
+    def test_net4_ip_rest(self, session_scope):
+        session_scope.__enter__ = Mock(return_value=Mock())
+        session_scope.__exit__ = Mock(return_value=None)
+
+        with patch.object(self.generator, 'get_provd_net4_ip') as provd_net4_ip,\
+                patch.object(self.generator, 'get_provd_net4_ip_rest') as provd_net4_ip_rest,\
+                patch.object(self.generator, 'get_netiface_net4_ip') as netiface_net4_ip,\
+                patch.object(self.generator, 'get_provd_http_port') as provd_http_port,\
+                patch.object(self.generator, 'get_provd_rest_port') as provd_rest_port:
+            provd_net4_ip.return_value = None
+            provd_net4_ip_rest.return_value = '10.0.0.254'
+            provd_http_port.return_value = None
+            provd_rest_port.return_value = None
+            netiface_net4_ip.return_value = None
+
+            value = self.generator.generate()
+
+        assert_config_equal(value, textwrap.dedent('''\
+            rest_api:
+                ip: 10.0.0.254
+        '''))
+
+    @patch('xivo_confgen.plugins.provd_conf.session_scope')
+    def test_complete_configuration(self, session_scope):
+        session_scope.__enter__ = Mock(return_value=Mock())
+        session_scope.__exit__ = Mock(return_value=None)
+
+        with patch.object(self.generator, 'get_provd_net4_ip') as provd_net4_ip,\
+                patch.object(self.generator, 'get_provd_net4_ip_rest') as provd_net4_ip_rest,\
+                patch.object(self.generator, 'get_netiface_net4_ip') as netiface_net4_ip,\
+                patch.object(self.generator, 'get_provd_http_port') as provd_http_port,\
+                patch.object(self.generator, 'get_provd_rest_port') as provd_rest_port:
+            provd_net4_ip.return_value = '10.0.0.254'
+            provd_net4_ip_rest.return_value = '10.0.0.253'
+            provd_http_port.return_value = 8666
+            provd_rest_port.return_value = 8667
+            netiface_net4_ip.return_value = '10.0.0.222'
+
+            value = self.generator.generate()
+
+        assert_config_equal(value, textwrap.dedent('''\
+            general:
+                external_ip: 10.0.0.254
+                http_port: 8666
+            rest_api:
+                ip: 10.0.0.253
+                port: 8667
+        '''))
+
+    @patch('xivo_confgen.plugins.provd_conf.session_scope')
     def test_no_net4_ip(self, session_scope):
         session_scope.__enter__ = Mock(return_value=Mock())
         session_scope.__exit__ = Mock(return_value=None)
 
-        with patch.object(self.generator, 'get_provd_net4_ip') as provd_net4_ip:
+        with patch.object(self.generator, 'get_provd_net4_ip') as provd_net4_ip,\
+                patch.object(self.generator, 'get_provd_net4_ip_rest') as provd_net4_ip_rest,\
+                patch.object(self.generator, 'get_provd_http_port') as provd_http_port,\
+                patch.object(self.generator, 'get_provd_rest_port') as provd_rest_port,\
+                patch.object(self.generator, 'get_netiface_net4_ip') as netiface_net4_ip:
             provd_net4_ip.return_value = None
+            provd_net4_ip_rest.return_value = None
+            provd_http_port.return_value = None
+            provd_rest_port.return_value = None
+            netiface_net4_ip.return_value = None
 
-            with patch.object(self.generator, 'get_netiface_net4_ip') as netiface_net4_ip:
-                netiface_net4_ip.return_value = None
+            value = self.generator.generate()
 
-                value = self.generator.generate()
-
-        assert_config_equal(value, '')
+        assert_config_equal(value, '{}')
